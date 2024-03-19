fea3a0daf2f2ae9e457b773b1f3f86db56ca7969
Nolan Cyr <nolangcyr@gmail.com>
2024-03-19 12:05:50 -0400

    final M3 updates

--- main.cpp
+++ main.cpp
@@ -312,6 +312,24 @@ void printTransferLines(vector<LineName> transfers) {
     }
 }
 
+string printTransferLinesAlternative(vector<LineName> transfers) {
+    unsigned int numTransfers = transfers.size();
+    string transferLinesStr = "";
+
+    if (numTransfers > 0) {
+        transferLinesStr += " (";
+        for (int i = 0; i < numTransfers; ++i) {
+            transferLinesStr += Line::getTextForEnum(transfers[i]);
+            if (i != numTransfers - 1) { // If it's not the last line, add a space
+                transferLinesStr += " ";
+            }
+        }
+        transferLinesStr += ") ";
+    }
+
+    return transferLinesStr;
+}
+
 void printAllStations(Train &train) {
     string currentStationID = train.getCurrentStation().getId();
     vector<Station> stations =  train.getScheduledStops();
@@ -327,11 +345,12 @@ void printAllStations(Train &train) {
             string stopsAwayText = stopsAway == 0 ? "" : (stopsAway == 1 ? "(Next Stop)" : "(" + to_string(stopsAway) + " stops away)");
 
             if (stations[i].getId() == currentStationID) {
-                cout << setw(30) << left << stations[i].getName() << "  **  Current Station  **" << endl;
+                cout << setw(30) << left << stations[i].getName() << "      (Current Station)" << endl;
                 cout << "    |" << endl;
             }
             else {
-                cout << setw(35) << left << stations[i].getName() << stopsAwayText << endl;
+                cout << setw(35) << left << stations[i].getName() << printTransferLinesAlternative(stations[i].getTransfers());
+                cout << " " << stopsAwayText << endl;
                 cout << "    |" << endl;
             }
         }
@@ -348,7 +367,8 @@ void printAllStations(Train &train) {
                 cout << "    |" << endl;
             }
             else {
-                cout << setw(35) << left << stations[i].getName() << stopsAwayText << endl;
+                cout << setw(35) << left << stations[i].getName() << printTransferLinesAlternative(stations[i].getTransfers());
+                cout << " " << stopsAwayText << endl;
                 cout << "    |" << endl;
             }
         }
